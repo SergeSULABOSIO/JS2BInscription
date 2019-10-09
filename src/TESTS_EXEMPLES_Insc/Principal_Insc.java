1a8b7f9c58bfc53d1f3125572aae45429a3552d6
Serge SULA <sulabosiog@gmail.com>
2019-10-09 21:26:22 +0100

    RAS

--- src/TESTS_EXEMPLES_Insc/Principal_Insc.java
+++ src/TESTS_EXEMPLES_Insc/Principal_Insc.java
@@ -76,6 +76,7 @@ public class Principal_Insc extends javax.swing.JFrame {
         //Chargement des Frais
         //Un très petit ajustement dd rien du tout.
         //RAS
+        //ENCORE RIEN
         this.listeMonnaies = new Vector<>();
         this.listeMonnaies.add(new Monnaie(12, entreprise.getId(), utilisateur.getId(), anneescolaire.getId(), "USD - Dollars", "USD", InterfaceMonnaie.NATURE_MONNAIE_ETRANGERE, 1620, 1010101010, InterfaceMonnaie.BETA_EXISTANT));
         this.listeMonnaies.add(new Monnaie(13, entreprise.getId(), utilisateur.getId(), anneescolaire.getId(), "CDF - Francs", "CDF", InterfaceMonnaie.NATURE_MONNAIE_LOCALE, 1, 0455, InterfaceMonnaie.BETA_EXISTANT));
