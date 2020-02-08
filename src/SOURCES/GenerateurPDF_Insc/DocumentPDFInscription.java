3113f4ad8cf414dc5daf991cd83c75231f9b1ecf
Serge SULA <sulabosiog@gmail.com>
2020-02-08 10:55:23 +0100

    Merge origin/master
    Conflicts:
    	build/classes/BEAN_BARRE_OUTILS/Bouton.class

--- src/SOURCES/GenerateurPDF_Insc/DocumentPDFInscription.java
+++ src/SOURCES/GenerateurPDF_Insc/DocumentPDFInscription.java
@@ -18,6 +18,7 @@ import Source.Interface.InterfaceMonnaie;
 import Source.Objet.Ayantdroit;
 import Source.Objet.Eleve;
 import Source.Objet.LiaisonFraisEleve;
+import Source.Objet.UtilObjet;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
@@ -98,8 +99,7 @@ public class DocumentPDFInscription extends PdfPageEventHelper {
 
     private void parametre_construire_fichier() {
         try {
-            String nomFichier = this.gestionnaireInscription.getNomfichierPreuve();
-            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(nomFichier));
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(UtilObjet.SYSTEM_FICHIER_OUTPUT_PDF));
             writer.setPageEvent(new MarqueS2B());
             this.document.open();
             setDonneesBibliographiques();
@@ -112,11 +112,7 @@ public class DocumentPDFInscription extends PdfPageEventHelper {
     }
 
     private void parametres_ouvrir_fichier() {
-        String nomFichier = "Output.pdf";
-        if (this.gestionnaireInscription != null) {
-            nomFichier = this.gestionnaireInscription.getNomfichierPreuve();
-        }
-        File fic = new File(nomFichier);
+        File fic = new File(UtilObjet.SYSTEM_FICHIER_OUTPUT_PDF);
         if (fic.exists() == true) {
             try {
                 Desktop.getDesktop().open(fic);
@@ -135,11 +131,7 @@ public class DocumentPDFInscription extends PdfPageEventHelper {
     }
 
     private void parametres_imprimer_fichier() {
-        String nomFichier = "Output.pdf";
-        if (this.gestionnaireInscription != null) {
-            nomFichier = this.gestionnaireInscription.getNomfichierPreuve();
-        }
-        File fic = new File(nomFichier);
+        File fic = new File(UtilObjet.SYSTEM_FICHIER_OUTPUT_PDF);
         if (fic.exists() == true) {
             try {
                 Desktop.getDesktop().print(fic);
@@ -244,15 +236,15 @@ public class DocumentPDFInscription extends PdfPageEventHelper {
             PdfPCell celluleLogoEntreprise = null;
             String logo = "";
             if (this.gestionnaireInscription != null) {
-                logo = this.gestionnaireInscription.getEntreprise().getLogo();
+                logo = (new File(this.gestionnaireInscription.getEntreprise().getLogo())).getName();
                 System.out.println("Fic logo: " + logo);
             }
-            File ficLogo = new File(new File(logo).getName());
+            File ficLogo = new File(UtilObjet.SYSTEM_USER_HOME+"/"+logo);
             System.out.println("Fichier Logo: " + ficLogo.getAbsolutePath());
             if (ficLogo.exists() == true) {
-                System.out.println("Fichier Logo: " + ficLogo.getAbsolutePath()+ " - Trouvé!");
+                //System.out.println("Fichier Logo: " + ficLogo.getAbsolutePath() + " - Trouvé!");
                 //Chargement du logo et redimensionnement afin que celui-ci convienne dans l'espace qui lui est accordé
-                Image Imglogo = Image.getInstance(ficLogo.getName());
+                Image Imglogo = Image.getInstance(ficLogo.getAbsolutePath());
                 Imglogo.scaleAbsoluteWidth(70);
                 Imglogo.scaleAbsoluteHeight(70);
                 celluleLogoEntreprise = new PdfPCell(Imglogo);
